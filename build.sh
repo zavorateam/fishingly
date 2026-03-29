5323535852870aec3b86838ae4b28845155d4238
zavorateam <155813894+zavorateam@users.noreply.github.com>
2026-03-29 14:51:19 +0300

    Minor changes

--- build.sh
+++ build.sh
@@ -1,106 +1,108 @@
 #!/bin/bash
 
 # Define RELEASE=1 before running to build a release.
+APP="fishingly"
 
 echo "Downloading and updating compiler..."
-if git clone https://github.com/Feodor0090/j2me_compiler.git 2>/dev/null
-then
+if git clone https://github.com/Feodor0090/j2me_compiler.git 2>/dev/null; then
   echo "Done."
 else
   echo "Already downloaded."
 fi
-cd j2me_compiler
+cd j2me_compiler || exit 1
 git pull
 cd ..
 
 # Manifest
-cp Application\ Descriptor manifest.mf
-echo -en "Commit: " >> manifest.mf
+cp "Application Descriptor" manifest.mf
+echo -n "Commit: " >> manifest.mf
 git rev-parse --short HEAD >> manifest.mf 2>/dev/null || echo "unknown" >> manifest.mf
 
-WORK_DIR=`dirname $0`
-cd ${WORK_DIR}
+WORK_DIR=$(dirname "$0")
+cd "${WORK_DIR}" || exit 1
 
 mkdir -p jar
 
-APP=sm-v1
-
 # STATIC VARS
-JAVA_HOME=./j2me_compiler/jdk1.6.0_45
-WTK_HOME=./j2me_compiler/WTK2.5.2
-PROGUARD=./j2me_compiler/proguard/bin/proguard.sh
-RES=res
-MANIFEST=manifest.mf
+JAVA_HOME="./j2me_compiler/jdk1.6.0_45"
+WTK_HOME="./j2me_compiler/WTK2.5.2"
+PROGUARD="./j2me_compiler/proguard/bin/proguard.sh"
+RES="res"
+MANIFEST="manifest.mf"
 PATHSEP=":"
-JAVAC=javac
-JAR=jar
 
-# DYNAMIC VARS
-LIB_DIR=${WTK_HOME}/lib
-CLDCAPI=${LIB_DIR}/cldcapi11.jar
-MIDPAPI=${LIB_DIR}/midpapi20.jar
-PREVERIFY=${WTK_HOME}/bin/preverify
-
-# Надежное формирование CLASSPATH (забирает jsr75, jsr135, jsr118 и др.)
-TCP=${LIB_DIR}/*
-CLASSPATH=`echo $TCP | sed "s/ /:/g"`
-
-if [ -n "${JAVA_HOME}" ] ; then
-  JAVAC=${JAVA_HOME}/bin/javac
-  JAR=${JAVA_HOME}/bin/jar
+if [ -n "${JAVA_HOME}" ] && [ -d "${JAVA_HOME}" ]; then
+  JAVAC="${JAVA_HOME}/bin/javac"
+  JAR="${JAVA_HOME}/bin/jar"
+else
+  JAVAC="javac"
+  JAR="jar"
 fi
 
+# DYNAMIC VARS
+LIB_DIR="${WTK_HOME}/lib"
+CLDCAPI="${LIB_DIR}/cldcapi11.jar"
+MIDPAPI="${LIB_DIR}/midpapi20.jar"
+PREVERIFY="${WTK_HOME}/bin/preverify"
+
+# Надежное формирование CLASSPATH через массивы bash (защита от пробелов в путях)
+shopt -s nullglob
+JARS=("${LIB_DIR}"/*.jar)
+CLASSPATH=$(IFS=${PATHSEP}; echo "${JARS[*]}")
+shopt -u nullglob
+
 # ACTION
-echo "Working on" ${APP}
+echo "Working on ${APP}"
 pwd
 echo "Creating or cleaning directories..."
-mkdir -p ./tmpclasses
-mkdir -p ./classes
-rm -rf ./tmpclasses/*
-rm -rf ./classes/*
+mkdir -p ./tmpclasses ./classes
+rm -rf ./tmpclasses/* ./classes/*
 
 echo "Compiling source files..."
-${JAVAC} \
-    -bootclasspath ${CLDCAPI}${PATHSEP}${MIDPAPI} \
+# Собираем все .java файлы
+find ./src -name '*.java' > sources.list
+
+"${JAVAC}" \
+    -bootclasspath "${CLDCAPI}${PATHSEP}${MIDPAPI}" \
     -source 1.3 \
     -target 1.3 \
     -d ./tmpclasses \
-    -classpath ./tmpclasses${PATHSEP}${CLASSPATH} \
-    `find ./src -name '*.java'`
+    -classpath "./tmpclasses${PATHSEP}${CLASSPATH}" \
+    @sources.list
     
-if [ $? -eq 0 ]
-then
+if [ $? -eq 0 ]; then
   echo "Compilation ok!"
 else
+  echo "Compilation failed!"
   exit 1
 fi
 
 echo "Preverifying class files..."
-${PREVERIFY} \
-    -classpath ${CLDCAPI}${PATHSEP}${MIDPAPI}${PATHSEP}${CLASSPATH}${PATHSEP}./tmpclasses \
+"${PREVERIFY}" \
+    -classpath "${CLDCAPI}${PATHSEP}${MIDPAPI}${PATHSEP}${CLASSPATH}${PATHSEP}./tmpclasses" \
     -d ./classes \
     ./tmpclasses
     
-if[ $? -eq 0 ]
-then
+if [ $? -eq 0 ]; then
   echo "Preverify ok!"
 else
+  echo "Preverify failed!"
   exit 1
 fi
 
 echo "Jaring preverified class files..."
-${JAR} cmf ${MANIFEST} ${APP}.jar -C ./classes .
+"${JAR}" cmf "${MANIFEST}" "${APP}.jar" -C ./classes .
 
-if [ -d ${RES} ] ; then
-  ${JAR} uf ${APP}.jar -C ${RES} .
+if [ -d "${RES}" ]; then
+  "${JAR}" uf "${APP}.jar" -C "${RES}" .
 fi
 
 echo "Build done! ./${APP}.jar"
 
 echo "Optimizing ${APP}..."
-chmod +x ${PROGUARD}
-touch cf.cfg
+chmod +x "${PROGUARD}"
 
+# Формируем конфиг ProGuard
 cat proguard.cfg > cf.cfg
 echo "-injars ./${APP}.jar" >> cf.cfg
 echo "-outjar ./${APP}_obf.jar" >> cf.cfg
@@ -108,8 +110,16 @@ echo "-printseeds ./${APP}_obf_seeds.txt" >> cf.cfg
 echo "-printmapping ./${APP}_obf_map.txt" >> cf.cfg
 echo "-libraryjars ${CLASSPATH}" >> cf.cfg
 
-${PROGUARD} @cf.cfg
+"${PROGUARD}" @cf.cfg
 
-# Копируем финализированные файлы в папку jar
-mv ${APP}.jar jar/
-mv ${APP}_obf.jar jar/
+# Перемещаем jar файлы
+mv "${APP}.jar" jar/
+mv "${APP}_obf.jar" jar/
+
+# Генерируем JAD файл для обфусцированного (релизного) JAR
+JAR_SIZE=$(stat -c%s "jar/${APP}_obf.jar" 2>/dev/null || wc -c < "jar/${APP}_obf.jar")
+cp "${MANIFEST}" "jar/${APP}.jad"
+echo "MIDlet-Jar-Size: ${JAR_SIZE}" >> "jar/${APP}.jad"
+echo "MIDlet-Jar-URL: ${APP}_obf.jar" >> "jar/${APP}.jad"
+
+echo "Done! Check the /jar folder."

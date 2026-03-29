afb5bede1c225d6cd64750094e3eec4980bb455c
zavorateam <155813894+zavorateam@users.noreply.github.com>
2026-03-29 14:46:45 +0300

    Initial commit

---
+++ src/symusic/FishingMIDlet.java
@@ -0,0 +1,33 @@
+package fishing;
+
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.lcdui.Display;
+
+public class FishingMIDlet extends MIDlet {
+    private FishingCanvas canvas;
+
+    public void startApp() {
+        if (canvas == null) {
+            canvas = new FishingCanvas(this);
+        }
+        Display.getDisplay(this).setCurrent(canvas);
+        canvas.start();
+    }
+
+    public void pauseApp() {
+        if (canvas != null) {
+            canvas.pause();
+        }
+    }
+
+    public void destroyApp(boolean unconditional) {
+        if (canvas != null) {
+            canvas.stop();
+        }
+    }
+    
+    public void exit() {
+        destroyApp(true);
+        notifyDestroyed();
+    }
+}
